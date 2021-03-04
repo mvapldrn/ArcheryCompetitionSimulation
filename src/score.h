98e60d990a0e0ba855ab85ca4247d2c8a7e74200
Marcel van Apeldoorn <mvapldrn@gmail.com>
2021-03-04 07:25:37 +0100

    Added GPL3 to headers

--- src/score.h
+++ src/score.h
@@ -2,7 +2,23 @@
 *** Name      : score.h                                                    ***
 *** Purpose   : Defines the types and routines for scoring model           ***
 *** Author    : Marcel van Apeldoorn <mvapldrn@gmail.com>                  ***
-*** Copyright : 2013                                                       ***
+*** Copyright : 2013-2021                                                  ***
+***                                                                        ***
+*** This file is part of ArcheryCompetitionSimulation.                     ***
+***                                                                        ***
+*** ArcheryCompetitionSimulation is free software: you can redistribute it ***
+*** and/or modify it under the terms of the GNU General Public License as  ***
+*** published by the Free Software Foundation, either version 3 of the     ***
+*** License, or (at your option) any later version.                        ***
+***                                                                        ***
+*** ArcheryCompetitionSimulation is distributed in the hope that it will   ***
+*** be useful, but WITHOUT ANY WARRANTY; without even the implied warranty ***
+*** of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       ***
+*** GNU General Public License for more details.                           ***
+***                                                                        ***
+*** You should have received a copy of the GNU General Public License      ***
+*** along with ArcheryCompetitionSimulation.  If not,                      ***
+*** see <https://www.gnu.org/licenses/>.                                   ***
 *****************************************************************************/
 
 #ifndef _SCORE_H
